b88b21b61d2508e509e205fe24a20d4f1d985580
Lanang-123 <lanangobey123@gmail.com>
2022-09-16 21:05:37 +0800

    Modifikasi galery dan benefit

--- components/Benefit.js
+++ components/Benefit.js
@@ -1,3 +1,7 @@
+import {MdEmojiPeople} from "react-icons/md"
+import {GiProgression} from "react-icons/gi"
+import {FiSettings} from "react-icons/fi"
+
 const Benefit = () => {
     return (
       <section className="text-gray-600 body-font relative bg-green-200">
@@ -13,85 +17,37 @@ const Benefit = () => {
           <div className="flex flex-wrap -m-4">
             <div className="p-4 md:w-1/3 group">
               <div className="flex rounded-lg h-full border group-hover:bg-green-500 group-hover:text-white bg-white shadow-lg p-8 flex-col">
-                <div className="flex items-center mb-3">
-                  <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full group-hover:bg-white group-hover:text-green-500 bg-green-500 text-white flex-shrink-0">
-                    <svg
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-                    </svg>
+                <div className="flex items-center mb-3 mx-auto">
+                  <div className="w-10 h-10 mr-3 inline-flex items-center justify-center rounded-full group-hover:bg-white group-hover:text-green-500 bg-green-500 text-white flex-shrink-0 ">
+                    <MdEmojiPeople className="w-8 h-8"/>
                   </div>
-                  <h2 className="text-gray-900 text-lg title-font font-medium group-hover:text-white">
-                    Shooting Stars
+                  <h2 className="text-gray-900 text-2xl title-font font-medium group-hover:text-white">
+                    Experience
                   </h2>
-                </div>
-                <div className="flex-grow">
-                  <p className="leading-relaxed text-base">
-                    Blue bottle crucifix vinyl post-ironic four dollar toast
-                    vegan taxidermy. Gastropub indxgo juice poutine.
-                  </p>
                 </div>
               </div>
             </div>
             <div className="p-4 md:w-1/3 group">
               <div className="flex rounded-lg h-full border group-hover:bg-green-500 group-hover:text-white bg-white shadow-lg p-8 flex-col">
-                <div className="flex items-center mb-3">
-                  <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full group-hover:bg-white group-hover:text-green-500 bg-green-500 text-white flex-shrink-0">
-                    <svg
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-                    </svg>
+                <div className="flex items-center mb-3 mx-auto">
+                  <div className="w-10 h-10 mr-3 inline-flex items-center justify-center rounded-full group-hover:bg-white group-hover:text-green-500 bg-green-500 text-white flex-shrink-0">
+                    <FiSettings className="w-8 h-8"/>
                   </div>
-                  <h2 className="text-gray-900 text-lg title-font font-medium group-hover:text-white">
-                    Shooting Stars
+                  <h2 className="text-gray-900 text-2xl title-font font-medium group-hover:text-white">
+                    Development
                   </h2>
-                </div>
-                <div className="flex-grow">
-                  <p className="leading-relaxed text-base">
-                    Blue bottle crucifix vinyl post-ironic four dollar toast
-                    vegan taxidermy. Gastropub indxgo juice poutine.
-                  </p>
                 </div>
               </div>
             </div>
             <div className="p-4 md:w-1/3 group">
               <div className="flex rounded-lg h-full border group-hover:bg-green-500 group-hover:text-white bg-white shadow-lg p-8 flex-col">
-                <div className="flex items-center mb-3">
-                  <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full group-hover:bg-white group-hover:text-green-500 bg-green-500 text-white flex-shrink-0">
-                    <svg
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-                    </svg>
+                <div className="flex items-center mb-3 mx-auto">
+                  <div className="w-10 h-10 mr-3 inline-flex items-center justify-center rounded-full group-hover:bg-white group-hover:text-green-500 bg-green-500 text-white flex-shrink-0">
+                    <GiProgression className="w-8 h-8"/>
                   </div>
-                  <h2 className="text-gray-900 text-lg title-font font-medium group-hover:text-white">
-                    Shooting Stars
+                  <h2 className="text-gray-900 text-2xl title-font font-medium group-hover:text-white">
+                    Sustain Ability
                   </h2>
-                </div>
-                <div className="flex-grow">
-                  <p className="leading-relaxed text-base">
-                    Blue bottle crucifix vinyl post-ironic four dollar toast
-                    vegan taxidermy. Gastropub indxgo juice poutine.
-                  </p>
                 </div>
               </div>
             </div>
--- pages/Galery.js
+++ pages/Galery.js
@@ -2,206 +2,101 @@ import React from "react";
 import Image from "next/image";
 
 const Galery = () => {
+  const dataFoto = [
+    {
+      id:1,
+      foto:"/assets/galery/psa/psa1.jpg"
+    },
+    {
+      id:2,
+      foto:"/assets/galery/psa/psa2.jpg"
+    },
+    {
+      id:3,
+      foto:"/assets/galery/psa/ps4.jpg"
+    },
+    {
+      id:5,
+      foto:"/assets/galery/psa/psa5.jpg"
+    },
+    {
+      id:6,
+      foto:"/assets/galery/psa/psa6.jpeg"
+    },
+    {
+      id:7,
+      foto:"/assets/galery/psa/psa7.jpeg"
+    },
+    {
+      id:8,
+      foto:"/assets/galery/psa/psa8.jpeg"
+    },
+    {
+      id:9,
+      foto:"/assets/galery/ss/ss1.jpg"
+    },
+    {
+      id:10,
+      foto:"/assets/galery/ss/ss2.jpg"
+    },
+    {
+      id:11,
+      foto:"/assets/galery/ss/ss3.jpg"
+    },
+    {
+      id:12,
+      foto:"/assets/galery/ss/ss4.jpg"
+    },
+    {
+      id:13,
+      foto:"/assets/galery/ss/ss5.jpg"
+    },
+    {
+      id:14,
+      foto:"/assets/galery/ss/ss6.jpg"
+    },
+    {
+      id:15,
+      foto:"/assets/galery/ss/ss7.jpeg"
+    },
+    {
+      id:16,
+      foto:"/assets/galery/ss/ss8.jpg"
+    },
+    {
+      id:17,
+      foto:"/assets/galery/ss/ss9.jpg"
+    },
+    {
+      id:18,
+      foto:"/assets/galery/ss/ss10.jpg"
+    },
+  ]
+
+
   return (
     <div>
       <section className="overflow-hidden text-gray-700 ">
         <div className="container px-5 py-2 mx-auto lg:pt-12 lg:px-32">
-          <h1 className="text-center text-slate-700 text-3xl mb-10">
+          <h1 className="text-center text-slate-700 text-5xl mb-10">
             Gallery Bali Surya Pratama
           </h1>
-          <div className="flex flex-wrap -m-1 md:-m-2">
-            <div className="flex flex-wrap w-1/3">
-              <div className="w-full p-1 md:p-2">
-                <Image
-                  alt="gallery"
-                  className="block object-cover object-center w-full h-full rounded-lg"
-                  width={600}
-                  height={300}
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-                />
-              </div>
-            </div>
-            <div className="flex flex-wrap w-1/3">
-              <div className="w-full p-1 md:p-2">
-                <Image
-                  alt="gallery"
-                  className="block object-cover object-center w-full h-full rounded-lg"
-                  width={600}
-                  height={300}
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-                />
-              </div>
-            </div>
-            <div className="flex flex-wrap w-1/3">
-              <div className="w-full p-1 md:p-2">
-                <Image
-                  alt="gallery"
-                  className="block object-cover object-center w-full h-full rounded-lg"
-                  width={600}
-                  height={300}
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-                />
-              </div>
-            </div>
-            <div className="flex flex-wrap w-1/3">
-              <div className="w-full p-1 md:p-2">
-                <Image
-                  alt="gallery"
-                  className="block object-cover object-center w-full h-full rounded-lg"
-                  width={600}
-                  height={300}
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-                />
-              </div>
-            </div>
-            <div className="flex flex-wrap w-1/3">
-              <div className="w-full p-1 md:p-2">
-                <Image
-                  alt="gallery"
-                  className="block object-cover object-center w-full h-full rounded-lg"
-                  width={600}
-                  height={300}
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-                />
-              </div>
-            </div>
-            <div className="flex flex-wrap w-1/3">
-              <div className="w-full p-1 md:p-2">
-                <Image
-                  alt="gallery"
-                  className="block object-cover object-center w-full h-full rounded-lg"
-                  width={600}
-                  height={300}
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-                />
-              </div>
-            </div>
-          </div>
         </div>
       </section>
 
-      <section className="overflow-hidden text-gray-700">
-        <div className="container px-5 py-2 mx-auto lg:pt-24 lg:px-32">
-          <div className="flex flex-wrap -m-1 md:-m-2">
-            <div className="flex flex-wrap w-1/2">
-              <div className="w-1/2 p-1 md:p-2">
-                <Image
-                  alt="gallery"
-                  className="block object-cover object-center w-full h-full rounded-lg"
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp"
-                  width={600}
-                  height={300}
-                />
-              </div>
-              <div className="w-1/2 p-1 md:p-2">
-                <Image
-                  alt="gallery"
-                  className="block object-cover object-center w-full h-full rounded-lg"
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"
-                  width={600}
-                  height={300}
-                />
-              </div>
-              <div className="w-full p-1 md:p-2">
-                <Image
-                  alt="gallery"
-                  className="block object-cover object-center w-full h-full rounded-lg"
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-                  width={600}
-                  height={300}
-                />
-              </div>
-            </div>
-            <div className="flex flex-wrap w-1/2">
-              <div className="w-full p-1 md:p-2">
-                <Image
-                  alt="gallery"
-                  className="block object-cover object-center w-full h-full rounded-lg"
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp"
-                  width={600}
-                  height={300}
-                />
-              </div>
-              <div className="w-1/2 p-1 md:p-2">
-                <Image
-                  alt="gallery"
-                  className="block object-cover object-center w-full h-full rounded-lg"
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp"
-                  width={600}
-                  height={300}
-                />
-              </div>
-              <div className="w-1/2 p-1 md:p-2">
-                <Image
-                  alt="gallery"
-                  className="block object-cover object-center w-full h-full rounded-lg"
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp"
-                  width={600}
-                  height={300}
-                />
-              </div>
-            </div>
-            <div className="flex flex-wrap w-1/2">
-              <div className="w-1/2 p-1 md:p-2">
-                <Image
-                  alt="gallery"
-                  className="block object-cover object-center w-full h-full rounded-lg"
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp"
-                  width={600}
-                  height={300}
-                />
-              </div>
-              <div className="w-1/2 p-1 md:p-2">
-                <Image
-                  alt="gallery"
-                  className="block object-cover object-center w-full h-full rounded-lg"
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"
-                  width={600}
-                  height={300}
-                />
-              </div>
-              <div className="w-full p-1 md:p-2">
-                <Image
-                  alt="gallery"
-                  className="block object-cover object-center w-full h-full rounded-lg"
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-                  width={600}
-                  height={300}
-                />
-              </div>
-            </div>
-            <div className="flex flex-wrap w-1/2">
-              <div className="w-full p-1 md:p-2">
-                <Image
-                  alt="gallery"
-                  className="block object-cover object-center w-full h-full rounded-lg"
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp"
-                  width={600}
-                  height={300}
-                />
-              </div>
-              <div className="w-1/2 p-1 md:p-2">
-                <Image
-                  alt="gallery"
-                  className="block object-cover object-center w-full h-full rounded-lg"
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp"
-                  width={600}
-                  height={300}
-                />
-              </div>
-              <div className="w-1/2 p-1 md:p-2">
-                <Image
-                  alt="gallery"
-                  className="block object-cover object-center w-full h-full rounded-lg"
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp"
-                  width={600}
-                  height={300}
-                />
-              </div>
-            </div>
+      <div class="container grid grid-cols-4 gap-5 mx-auto justify-center my-5">
+      {dataFoto.map(data => (
+          <div class="w-full rounded" key={data.id}>
+            <Image 
+              src={data.foto}
+              alt="image" 
+              width={380} 
+              height={380} 
+            />
           </div>
-        </div>
-      </section>
+      ))}
+      
+      </div>
     </div>
   );
 };
