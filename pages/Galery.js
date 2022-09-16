f86159c9ce9ad3ad29789beb839a01a622088466
Lanang-123 <62416075+Lanang-123@users.noreply.github.com>
2022-09-16 21:49:47 +0800

    Update Galery.js

--- pages/Galery.js
+++ pages/Galery.js
@@ -5,71 +5,71 @@ const Galery = () => {
   const dataFoto = [
     {
       id:1,
-      foto:"./assets/galery/psa/psa1.jpg"
+      foto:"/assets/galery/psa/psa1.jpg"
     },
     {
       id:2,
-      foto:"./assets/galery/psa/psa2.jpg"
+      foto:"/assets/galery/psa/psa2.jpg"
     },
     {
       id:3,
-      foto:"./assets/galery/psa/ps4.jpg"
+      foto:"/assets/galery/psa/ps4.jpg"
     },
     {
       id:5,
-      foto:"./assets/galery/psa/psa5.jpg"
+      foto:"/assets/galery/psa/psa5.jpg"
     },
     {
       id:6,
-      foto:"./assets/galery/psa/psa6.jpeg"
+      foto:"/assets/galery/psa/psa6.jpeg"
     },
     {
       id:7,
-      foto:"./assets/galery/psa/psa7.jpeg"
+      foto:"/assets/galery/psa/psa7.jpeg"
     },
     {
       id:8,
-      foto:"./assets/galery/psa/psa8.jpeg"
+      foto:"/assets/galery/psa/psa8.jpeg"
     },
     {
       id:9,
-      foto:"./assets/galery/ss/ss1.jpg"
+      foto:"/assets/galery/ss/ss1.jpg"
     },
     {
       id:10,
-      foto:"./assets/galery/ss/ss2.jpg"
+      foto:"/assets/galery/ss/ss2.jpg"
     },
     {
       id:11,
-      foto:"./assets/galery/ss/ss3.jpg"
+      foto:"/assets/galery/ss/ss3.jpg"
     },
     {
       id:12,
-      foto:"./assets/galery/ss/ss4.jpg"
+      foto:"/assets/galery/ss/ss4.jpg"
     },
     {
       id:13,
-      foto:"./assets/galery/ss/ss5.jpg"
+      foto:"/assets/galery/ss/ss5.jpg"
     },
     {
       id:14,
-      foto:"./assets/galery/ss/ss6.jpg"
+      foto:"/assets/galery/ss/ss6.jpg"
     },
     {
       id:15,
-      foto:"./assets/galery/ss/ss7.jpeg"
+      foto:"/assets/galery/ss/ss7.jpeg"
     },
     {
       id:16,
-      foto:"./assets/galery/ss/ss8.jpg"
+      foto:"/assets/galery/ss/ss8.jpg"
     },
     {
       id:17,
-      foto:"./assets/galery/ss/ss9.jpg"
+      foto:"/assets/galery/ss/ss9.jpg"
     },
     {
       id:18,
-      foto:"./assets/galery/ss/ss10.jpg"
+      foto:"/assets/galery/ss/ss10.jpg"
     },
   ]
 
