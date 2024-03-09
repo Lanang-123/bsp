9a8e9ea8a3691e02d35f1c8239771e9ea8cd1753
Lanang-123 <62416075+Lanang-123@users.noreply.github.com>
2024-03-08 19:28:14 -0800

    Update desc1

--- pages/kategori/[name].js
+++ pages/kategori/[name].js
@@ -20,7 +20,7 @@ const KategoriName = () => {
     {
       id: 1,
       title: "Filter Tank",
-      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+      desc: "desc1",
       img: {
         produk: "../assets/img/produk1.jpg",
         bahanBaku: "",
